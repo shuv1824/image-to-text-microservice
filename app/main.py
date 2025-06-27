118420c78e833c2c7ad93b2558284ab4d0dd62a4
Shah Nawaz <shuvo1824@hotmail.com>
2025-06-27 23:31:26 +0600

    chore: jinja templating added

--- app/main.py
+++ app/main.py
@@ -1,9 +1,13 @@
-from fastapi import FastAPI
+import pathlib
+from fastapi import FastAPI, Request
+from fastapi.responses import HTMLResponse
+from fastapi.templating import Jinja2Templates
 
-
+BASE_DIR = pathlib.Path(__file__).resolve().parent
 app = FastAPI()
+templates = Jinja2Templates(directory=str(BASE_DIR / "templates"))
 
 
-@app.get("/")
-def home_view():
-    return {"message": "Hello World"}
+@app.get("/", response_class=HTMLResponse)
+def home_view(request: Request):
+    return templates.TemplateResponse("home.html", {"request": request})
