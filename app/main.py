c3b03e82c321622b0416761e155ab515f2b15e5e
Shah Nawaz <shuvo1824@hotmail.com>
2025-07-03 23:05:05 +0600

    chore: settings from .env file

--- app/main.py
+++ app/main.py
@@ -1,7 +1,25 @@
 import pathlib
-from fastapi import FastAPI, Request
+from functools import lru_cache
+from fastapi import Depends, FastAPI, Request
 from fastapi.responses import HTMLResponse
 from fastapi.templating import Jinja2Templates
+from pydantic_settings import BaseSettings
+
+
+class Settings(BaseSettings):
+    debug: bool = False
+
+    class Config:
+        env_file = ".env"
+
+
+@lru_cache
+def get_settings():
+    return Settings()
+
+
+settings = get_settings()
+DEBUG = settings.debug
 
 BASE_DIR = pathlib.Path(__file__).resolve().parent
 app = FastAPI()
@@ -9,5 +27,5 @@ templates = Jinja2Templates(directory=str(BASE_DIR / "templates"))
 
 
 @app.get("/", response_class=HTMLResponse)
-def home_view(request: Request):
+def home_view(request: Request, settings: Settings = Depends(get_settings)):
     return templates.TemplateResponse("home.html", {"request": request})
